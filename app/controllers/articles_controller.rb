5ff082a0241d2150b06d4535cf3bdc6ae049b4ba
Nayan Admuthe <nayan.a@idexcel.com>
2024-04-12 15:49:19 +0530

    New Testing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,38 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article=Article.new
+    end
+
+    def edit
+        @article=Article.find(params[:id])
     end
 
     def create
         @article= Article.new(params.require(:article).permit(:title, :description))
-        @article.save
+        if @article.save
+            flash[:notice] = "Article was successfully created"
         redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice]= "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        puts "=============#{params}============"
+        @article=Article.find(params[:id])
+        puts "=========#{@article}================"
+        @article.destroy
+        redirect_to articles_path
     end
 end
